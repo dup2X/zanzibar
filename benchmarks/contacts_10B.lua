5eda26f1a10b2979258912329e62d74bbfed4e8d
Michael Sindelar <sindelar@uber.com>
2017-05-25 12:09:10 -0700

    add helper method for tertiary assignment

--- benchmarks/contacts_10B.lua
+++
@@ -1,5 +0,0 @@
--- wrk -t12 -c400 -d30s -s ./benchmarks/contacts_10B.lua http://localhost:8093/contacts/foo/contacts
--- go-torch -u http://localhost:8093/ -t5
-wrk.method = "POST"
-wrk.body = "{\"userUUID\":\"some-uuid\", \"contacts\":[]}"
-
--- benchmarks/health_0B.lua
+++
@@ -1,4 +0,0 @@
--- wrk -t12 -c400 -d30s -s ./benchmarks/health_0B.lua http://localhost:8093/health
--- go-torch -u http://localhost:8093/ -t5
-wrk.method = "GET"
-
